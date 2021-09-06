3311caef5a441d2bc8d3d09348d5e2aa8c62a2c3
samuelqlopes <78562787+samuelqlopes@users.noreply.github.com>
2021-09-06 00:10:23 -0300

    Adicionado questão 1 TI2

---
+++ soma_dois_numeros/src/soma_dois_numeros/Soma2numeros.java
@@ -0,0 +1,24 @@
+package soma_dois_numeros;
+
+import java.util.*;
+
+
+class Soma2numeros {
+	public static Scanner sc = new Scanner(System.in);
+			
+	public static void main(String args[]) throws Exception {
+			int num1, num2, soma;
+			
+			//Leitura
+			System.out.println("Digite um número");
+			num1 = sc.nextInt();
+			System.out.println("Digite outro número");
+			num2 = sc.nextInt();
+		
+			//Somar
+			soma = num1 + num2;
+			
+			//Mostrar na tela
+			System.out.println("Soma:" + soma);
+		}
+}
